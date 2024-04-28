92d0bd36c2485d961435c982c3d16eca14f859b1
Tamta Kakhidze <tamta.kakhidze@gmail.com>
2024-04-28 23:04:40 +0400

    Reviewing code and finalizing it

--- Functions_PHP/functions_for_testimonials.php
+++ Functions_PHP/functions_for_testimonials.php
@@ -18,28 +18,3 @@
     </div>";
 };
 ?>
-
-
-
-
-          <!-- Carousel Item 3 -->
-        <!-- <div class="carousel-item">
-            <div class="box">
-                <div class="client_info">
-                    <div class="client_name">
-                        <h5>
-                            Brad Johns
-                        </h5>
-                        <h6>
-                            Default model text
-                        </h6>
-                    </div>
-                    <i class="fa fa-quote-left" aria-hidden="true"></i>
-                </div>
-              <p>
-                Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy, editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Variouseditors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various
-              </p>
-            </div>
-          </div> -->
-        
-        <!-- end of Carousel Item 3 -->
